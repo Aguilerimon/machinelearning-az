0baa8ae0cfd4322d86cb3fe4508bc87fd2a9362b
Aguilera Monroy <antonio.aguilerimon@gmail.com>
2019-12-27 16:04:27 -0600

    Escalado de datos

--- datasets/Part 1 - Data Preprocessing/Section 2 -------------------- Part 1 - Data Preprocessing --------------------/Ejercicios/Plantilla_Preprocesado_de_Datos.R
+++ datasets/Part 1 - Data Preprocessing/Section 2 -------------------- Part 1 - Data Preprocessing --------------------/Ejercicios/Plantilla_Preprocesado_de_Datos.R
@@ -10,3 +10,32 @@ dataset$Age = ifelse(is.na(dataset$Age),
 dataset$Salary = ifelse(is.na(dataset$Salary),
                      ave(dataset$Salary, FUN = function(x) mean(x,na.rm = TRUE)), 
                      dataset$Salary)
+
+#Codificar variables categóricas
+dataset$Country = factor(dataset$Country,
+                         levels = c("France", "Spain", "Germany"),
+                         labels = c(1, 2, 3))
+
+dataset$Purchased = factor(dataset$Purchased,
+                           levels = c("No", "Yes"),
+                           labels = c(0, 1))
+
+#Dividir los datos en conjunto de entrenamiento y evaluación
+#Importamos la libreria para separar los conjuntos
+library(caTools)
+
+#Especificamos el numero de repeticiones del algoritmo (Puede ser cualquiera)
+set.seed(123)
+
+#Devuelve un vector del mismo tamaño que la variable o vector que se le da
+#indicando si debe formar parte del conjunto de entrenamiento
+split = sample.split(dataset$Purchased, SplitRatio = 0.8)
+
+#Separa y almacena en dos diferentes variables los conjuntos de 
+#entrenamiento y evaluación
+training_set = subset(dataset, split==TRUE)
+testing_set = subset(dataset, split==FALSE)
+
+#Escalado de variables
+training_set[,2:3] = scale(training_set[,2:3])
+testing_set[,2:3] = scale(testing_set[,2:3])
--- datasets/Part 1 - Data Preprocessing/Section 2 -------------------- Part 1 - Data Preprocessing --------------------/Ejercicios/Plantilla_Preprocesado_de_Datos.py
+++ datasets/Part 1 - Data Preprocessing/Section 2 -------------------- Part 1 - Data Preprocessing --------------------/Ejercicios/Plantilla_Preprocesado_de_Datos.py
@@ -31,3 +31,34 @@ imputer = Imputer(missing_values = "NaN", strategy = "mean", axis = 0)
 
 imputer = imputer.fit(x[:, 1:3])
 x[:, 1:3] = imputer.transform(x[:, 1:3])
+
+#Codificar los datos categóricos
+#Import de las librerias LabelEncoder(Le da igual si los datos son catogórico u ordinales)
+#Y la libreria OneHotEncoder(Implementación de las variable dummy categóricas)
+from sklearn.preprocessing import LabelEncoder, OneHotEncoder
+
+#Constructor del método labelencoder
+labelencoder_x = LabelEncoder()
+labelencoder_y = LabelEncoder()
+
+#Aplicamos la transformación numérica a la primer columna y la sobreescribimos al objeto x
+x[:,0] = labelencoder_x.fit_transform(x[:,0])
+
+#Para los valores boolean no nos interesa utilizar el método OneHotEncoder 
+y = labelencoder_y.fit_transform(y)
+
+#Es necesario haber tratado anteriormente el vector con LabelEncoder
+#debido a que OneHotEncoder no podrá convertir el string original a float
+onehotencoder = OneHotEncoder(categorical_features=[0])
+x = onehotencoder.fit_transform(x).toarray()
+
+#Dividir el dataset en un conjunto de entrenamiento y de evaluación
+#Importamos la sublibreria train_test_split
+from sklearn.cross_validation import train_test_split
+x_train, x_test, y_train, y_test = train_test_split(x,y, test_size= 0.2, random_state=0)
+
+#Escalado de variables
+from sklearn.preprocessing import StandardScaler
+sc_x = StandardScaler()
+x_train = sc_x.fit_transform(x_train)
+x_test = sc_x.transform(x_test)
